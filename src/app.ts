f603c1673126dc34748045c1e22fa60fc0c0cce2
Ashley Travaini <ashleytravaini@hotmail.com>
2022-09-10 08:34:24 +1000

    Added DB connection and basic setup of ZOD middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+import connectDB from './util/connectDB';
 import homeHandler from './handler/home.handler';
 import signupHandler from './handler/signup.handler';
 import loginHandler from './handler/login.handler';
@@ -9,6 +11,8 @@ import gameLogHandler from './handler/gameLog.handler';
 import gamesHandler from './handler/games.handler';
 
 dotenv.config();
+
+connectDB();
 
 const app: Express = express();
 const port = process.env.PORT;
@@ -21,6 +25,9 @@ app.use('/game', gameHandler);
 app.use('/games', gamesHandler);
 app.use('/game-log', gameLogHandler);
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+mongoose.connection.once('connected', () => {
+    console.log('⚡️[server]: Connected to MongoDB.');
+    app.listen(port, () => {
+        console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    });
+})
